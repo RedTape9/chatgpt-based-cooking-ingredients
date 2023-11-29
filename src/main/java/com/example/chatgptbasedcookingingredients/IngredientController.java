182d71a017e859cad8553fcde4319f5828fc7f13
Sergej Jaudszims <redtape9688@gmail.com>
2023-11-29 13:00:21 +0100

    change prompt

--- src/main/java/com/example/chatgptbasedcookingingredients/IngredientController.java
+++ src/main/java/com/example/chatgptbasedcookingingredients/IngredientController.java
@@ -20,7 +20,7 @@ public class IngredientController {
     @PostMapping(value = "/categorize", consumes = MediaType.TEXT_PLAIN_VALUE, produces = MediaType.TEXT_PLAIN_VALUE)
     public Mono<String> categorizeIngredient(@RequestBody String ingredient) {
         return webClient.post()
-                .bodyValue(new ChatGPTRequest("Kategorisiere die Zutat: " + ingredient))
+                .bodyValue(new ChatGPTRequest("Ist die Zutat:" + ingredient + " vegan, vegetarian oder regular?" ))
                 .retrieve()
                 .bodyToMono(ChatGPTResponse.class)
                 .map(ChatGPTResponse::text);
